d4e30f8e3f82862529565c3d0e21d587e4e49d25
zaidfar <zf5@njit.edu>
2021-10-14 10:51:09 -0400

    more housecleaning

--- lib/functions.php
+++ lib/functions.php
@@ -36,8 +36,12 @@ function is_valid_email($email = "")
     return filter_var(trim($email), FILTER_VALIDATE_EMAIL);
 }
 //TODO 3: User Helpers
-function is_logged_in()
+function is_logged_in($redirect = false, $destination = "login.php")
 {
+    if ($redirect) {
+        flash("You must be logged in to view this page", "warning");
+        die(header("Location: $destination"));
+    }
     return isset($_SESSION["user"]); //se($_SESSION, "user", false, false);
 }
 function has_role($role)
@@ -98,7 +102,6 @@ function reset_session()
 {
     session_unset();
     session_destroy();
-    session_start();
 }
 function users_check_duplicate($errorInfo)
 {
