49d82e8c1b73e755ced967657034ea80e012b122
zaidfar <zf5@njit.edu>
2021-10-14 10:20:14 -0400

    adding files to this branch

--- public_html/project/logout.php
+++ public_html/project/logout.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
+require(__DIR__ . "/../../lib/functions.php");
+reset_session();
+
+flash("Successfully logged out", "success");
 header("Location: login.php");
