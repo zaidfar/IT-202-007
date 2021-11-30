47f40270597b45fd2e9c4a747bd4d57bf0aafb1a
zaidfar <zf5@njit.edu>
2021-11-30 16:20:01 -0500

    second issue finished

--- public_html/project/sql/006_create_table_items.sql
+++ public_html/project/sql/006_create_table_items.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS Products(
     id int AUTO_INCREMENT PRIMARY  KEY,
     name varchar(30) UNIQUE,
     description text,
-    category varchar(30) UNIQUE,
+    category varchar(30),
     stock int DEFAULT  0,
     cost int DEFAULT  99999,
     image text,
